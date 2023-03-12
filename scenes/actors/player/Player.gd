593f18da4f943490a351afe3c8d0c15e1f7e12f4
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-12 00:19:22 -0500

    made slopes slopier

--- scenes/actors/player/Player.gd
+++ scenes/actors/player/Player.gd
@@ -16,7 +16,6 @@ const bulletPath = preload('res://scenes/actors/player/Bullet.tscn')
 
 var AMMO = 0
 var cursorVector = Vector2.ZERO
-var notTouchedWall = false
 var onSlope = false
 var steepness = 0
 
@@ -25,15 +24,19 @@ func _ready() -> void:
 
 func _physics_process(_delta) -> void:
 	apply_gravity()
+	
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	#Handling slopes
 	steepness = get_steepness()
-	if steepness > 44.0:
+	if abs(steepness) > 44.0:
 		onSlope = true
+
 	else:
 		onSlope = false
+		
+	print(rad_to_deg(get_floor_angle()))
 	
 	#Animations
 	if onSlope:
@@ -45,17 +48,18 @@ func _physics_process(_delta) -> void:
 		animatedSprite.speed_scale = 1
 
 	if is_on_floor():
-		notTouchedWall = true
 		if not onSlope:
 			if input.x == 0:
 				apply_friction()
 			else:
 				apply_acceleration(input.x)
+		else:
+			if steepness < 0:
+				velocity.x = move_toward(velocity.x, 0.75 * 800000, ACCELERATION)
+			elif steepness > 0:
+				velocity.x = move_toward(velocity.x, -0.75 * 800000, ACCELERATION)
+			velocity.y = move_toward(velocity.y, 1 * 800000, GRAVITY)
 
-	if is_on_wall_only() and notTouchedWall and steepness > 46:
-		velocity = Vector2.ZERO
-		await get_tree().create_timer(.1, false).timeout
-		notTouchedWall = false
 
 	cursorVector = get_global_mouse_position() - position
 
@@ -64,7 +68,6 @@ func _physics_process(_delta) -> void:
 			shoot()
 	if  AMMO < MAX_AMMO and is_on_floor() and timer.is_stopped() and not onSlope:
 		AMMO = MAX_AMMO
-		
 
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().reload_current_scene()
@@ -80,7 +83,7 @@ func apply_friction() -> void:
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(amount) -> void:
-	velocity.x = move_toward(get_velocity().x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
 
 func shoot() -> void:
 	velocity += cursorVector.normalized() * JUMP_FORCE
@@ -88,7 +91,7 @@ func shoot() -> void:
 		var bullet = bulletPath.instantiate()
 		bullet.position = $Marker2D.global_position
 		bullet.rotation = cursorVector.angle()
-		var randomBulletSpeed = Vector2(2000 + randf_range(-200, 200), 0)
+		var randomBulletSpeed = Vector2(2000 + randf_range(-400, 400), 0)
 		var randomAngle = cursorVector.angle() + randf_range(-0.25, 0.25)
 		bullet.apply_impulse(randomBulletSpeed.rotated(randomAngle), Vector2.ZERO)
 		get_parent().add_child(bullet)
@@ -96,4 +99,4 @@ func shoot() -> void:
 	AMMO -= 1
 
 func get_steepness() -> float:
-	return abs((-90) - rad_to_deg(slopeRayCast.get_collision_normal().angle()))
+	return (-90) - rad_to_deg(slopeRayCast.get_collision_normal().angle())
--- scenes/levels/DebugUI.gd
+++ scenes/levels/DebugUI.gd
@@ -7,3 +7,5 @@ func _physics_process(_delta):
 	$DebugMargin/Panel/VBoxContainer/SlopeDebug.text = "Slope: %s" % str(player.onSlope)
 	$DebugMargin/Panel/VBoxContainer/SteepnessDebug.text = "Steepness: %s" % str(player.get_steepness())
 	$DebugMargin/Panel/VBoxContainer/AmmoDebug.text = "Ammo: %s" % str(player.AMMO)
+	$DebugMargin/Panel/VBoxContainer/OnFloorDebug.text = "On Floor: %s" % str(player.is_on_floor())
+	$DebugMargin/Panel/VBoxContainer/OnWallDebug.text = "On Wall: %s" % str(player.is_on_wall())

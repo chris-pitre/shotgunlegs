bd9d1116bcaae7e0748d05a88b6b60e912f8b9f7
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-12 00:44:38 -0500

    refactored code to be more readable

--- scenes/actors/player/Player.gd
+++ scenes/actors/player/Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const bulletPath = preload('res://scenes/actors/player/Bullet.tscn')
+const SPEED_CAP = 80000
 
 @export var JUMP_FORCE: int = -140
 @export var MAX_SPEED: int = 100
@@ -23,21 +24,13 @@ func _ready() -> void:
 	animatedSprite.play("idle")
 
 func _physics_process(_delta) -> void:
-	apply_gravity()
-	
-	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	
 	#Handling slopes
 	steepness = get_steepness()
 	if abs(steepness) > 44.0:
 		onSlope = true
-
 	else:
 		onSlope = false
-		
-	print(rad_to_deg(get_floor_angle()))
-	
+			
 	#Animations
 	if onSlope:
 		animatedSprite.play("roll")
@@ -47,45 +40,34 @@ func _physics_process(_delta) -> void:
 		animatedSprite.play("idle")
 		animatedSprite.speed_scale = 1
 
-	if is_on_floor():
-		if not onSlope:
-			if input.x == 0:
-				apply_friction()
-			else:
-				apply_acceleration(input.x)
-		else:
-			if steepness < 0:
-				velocity.x = move_toward(velocity.x, 0.75 * 800000, ACCELERATION)
-			elif steepness > 0:
-				velocity.x = move_toward(velocity.x, -0.75 * 800000, ACCELERATION)
-			velocity.y = move_toward(velocity.y, 1 * 800000, GRAVITY)
-
-
-	cursorVector = get_global_mouse_position() - position
-
-	if AMMO > 0:
-		if timer.is_stopped() and Input.is_action_just_pressed("mouse1"):
-			shoot()
-	if  AMMO < MAX_AMMO and is_on_floor() and timer.is_stopped() and not onSlope:
-		AMMO = MAX_AMMO
-
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	apply_gravity()
+	do_movement(input.x)
+	shoot_and_reload()
+	
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().reload_current_scene()
 	
 	move_and_slide()
 	
 
-
+## Applies gravity to player velocity
 func apply_gravity() -> void:
 	velocity.y += GRAVITY
 
+## Applies friction to player velocity
 func apply_friction() -> void:
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
+## Applies acceleration to player velocity
 func apply_acceleration(amount) -> void:
 	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
 
+## Shoots shotgun and applys force in opposite direction
 func shoot() -> void:
+	cursorVector = get_global_mouse_position() - position
 	velocity += cursorVector.normalized() * JUMP_FORCE
 	for _i in range(10):	
 		var bullet = bulletPath.instantiate()
@@ -98,5 +80,30 @@ func shoot() -> void:
 	timer.start(0.25)
 	AMMO -= 1
 
+## Gets angle of slope from raycast
 func get_steepness() -> float:
 	return (-90) - rad_to_deg(slopeRayCast.get_collision_normal().angle())
+
+## Handles if player is able to shoot and reloads gun on the ground
+func shoot_and_reload() -> void:
+	if AMMO > 0:
+		if timer.is_stopped() and Input.is_action_just_pressed("mouse1"):
+			shoot()
+	if  AMMO < MAX_AMMO and is_on_floor() and timer.is_stopped() and not onSlope:
+		AMMO = MAX_AMMO
+
+## Does movement based off input axis if player is on floor and not on a slope
+## Otherwise, handles tumbling movement off a slope
+func do_movement(input) -> void:
+	if is_on_floor():
+		if not onSlope:
+			if input == 0:
+				apply_friction()
+			else:
+				apply_acceleration(input)
+		else:
+			if steepness < 0:
+				velocity.x = move_toward(velocity.x, 0.75 * SPEED_CAP, ACCELERATION)
+			elif steepness > 0:
+				velocity.x = move_toward(velocity.x, -0.75 * SPEED_CAP, ACCELERATION)
+			velocity.y = move_toward(velocity.y, 1 * 800000, GRAVITY)

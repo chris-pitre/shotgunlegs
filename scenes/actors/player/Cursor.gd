790f83ebefb0bebedd74d6ca461d880aa8a5c607
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-12 14:22:29 -0400

    added cursor animation

--- scenes/actors/player/Cursor.gd
+++ scenes/actors/player/Cursor.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+@onready var icon = $Icon
+
+func _ready():
+	$Icon.play()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 func _process(_delta):
 	global_position = get_global_mouse_position()

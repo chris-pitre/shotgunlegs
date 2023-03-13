37ac07b2665fa00cca84815b6d8f895316093990
rkpham <trumpkp@gmail.com>
2023-03-13 17:24:59 -0500

    Add punching snowman, some more level design testing

---
+++ scenes/actors/snowman_punch.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+@export var punch_force: Vector2 = Vector2(-128, 0)
+
+var punching = false
+
+func _on_area_2d_body_entered(_body):
+	if "velocity" in _body:
+		if not punching:
+			punching = true
+			play("punch")
+			await get_tree().create_timer((1.0/12.0) * 7).timeout
+			for body in $Area2D.get_overlapping_bodies():
+				if "velocity" in body:
+					body.velocity += punch_force + Vector2(0, -256)
+			await animation_finished
+			punching = false
--- scenes/levels/DebugUI.gd
+++ scenes/levels/DebugUI.gd
@@ -9,3 +9,4 @@ func _physics_process(_delta):
 	$DebugMargin/Panel/VBoxContainer/AmmoDebug.text = "Ammo: %s" % str(player.AMMO)
 	$DebugMargin/Panel/VBoxContainer/OnFloorDebug.text = "On Floor: %s" % str(player.is_on_floor())
 	$DebugMargin/Panel/VBoxContainer/OnWallDebug.text = "On Wall: %s" % str(player.is_on_wall())
+	$DebugMargin/Panel/VBoxContainer/FPS.text = "FPS: %s" % Engine.get_frames_per_second()

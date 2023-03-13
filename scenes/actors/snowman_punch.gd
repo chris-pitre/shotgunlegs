71f598e8ffa8a1cf78e70877da2ae128683c493f
rkpham <trumpkp@gmail.com>
2023-03-13 17:50:26 -0500

    fixed snowman punch, basic tilemap

--- scenes/actors/snowman_punch.gd
+++ scenes/actors/snowman_punch.gd
@@ -7,11 +7,17 @@ var punching = false
 func _on_area_2d_body_entered(_body):
 	if "velocity" in _body:
 		if not punching:
-			punching = true
-			play("punch")
-			await get_tree().create_timer((1.0/12.0) * 7).timeout
-			for body in $Area2D.get_overlapping_bodies():
-				if "velocity" in body:
-					body.velocity += punch_force + Vector2(0, -256)
-			await animation_finished
-			punching = false
+			punch()
+
+func punch() -> void:
+	punching = true
+	play("punch")
+	await get_tree().create_timer((1.0/12.0) * 7).timeout
+	for body in $Area2D.get_overlapping_bodies():
+		if "velocity" in body:
+			body.velocity += punch_force + Vector2(0, -256)
+	await animation_finished
+	punching = false
+	for body in $Area2D.get_overlapping_bodies():
+		if "velocity" in body:
+			punch()

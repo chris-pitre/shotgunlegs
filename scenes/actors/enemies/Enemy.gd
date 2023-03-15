0488a4c78f98bfe5251f741a2b966898b632fa1b
chris-pitre <c.tophanp3@gmail.com>
2023-03-15 16:41:29 -0400

    signals for hit detection

--- scenes/actors/enemies/Enemy.gd
+++ scenes/actors/enemies/Enemy.gd
@@ -1,4 +1,19 @@
 extends CharacterBody2D
 
-func _on_hitbox_body_entered(body):
-	pass 
+class_name Enemy
+
+@export var HP: int = 5
+
+@onready var hurtbox = $Hurtbox
+
+func _on_hitbox_body_entered(body) -> void:
+	pass
+
+func _on_hurtbox_body_entered(body) -> void:
+	if body is Bullet:
+		HP -= 1
+		for bullets in get_tree().get_nodes_in_group("Bullet"):
+			if body == bullets:
+				bullets.emit_signal("target_hit")
+		if HP <= 0:
+			queue_free()  

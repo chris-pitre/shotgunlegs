c390aef90a514e9ce6dd983c5c8c2043bb3e64da
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-23 15:58:35 -0400

    added blackout effect when changing scenes

--- scenes/prefabs/ChangeLevelZone.gd
+++ scenes/prefabs/ChangeLevelZone.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 @onready var area = $CollisionShape2D
+@onready var blackout = $Sprite2D
 
 @export var next_level: Resource 
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		var tween = get_tree().create_tween()
+		tween.set_trans(Tween.TRANS_CIRC)
+		await tween.tween_property(blackout, "modulate", Color(0, 0, 0, 1), 0.25).finished
 		get_tree().change_scene_to_file(next_level.resource_path)
+
 

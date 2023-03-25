6fbe8d674428ae586d11e721c93219c3d8479fa1
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-24 22:55:50 -0400

    main menu skeleton

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,5 @@
+extends MarginContainer
+
+@onready var start = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/Start
+@onready var options = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/Options
+@onready var quit = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/Quit
